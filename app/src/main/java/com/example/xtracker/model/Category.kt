28267940b3e46ecd34816e29f2f09c081e972606
Orhan Huseinbegovic <orhan.huseinbegovic@stu.ibu.edu.ba>
2024-05-20 21:38:43 +0200

    categories and types

---
+++ app/src/main/java/com/example/xtracker/model/Category.kt
@@ -0,0 +1,10 @@
+package com.example.xtracker.model
+
+
+import androidx.annotation.DrawableRes
+
+data class Category(
+    val id : Int,
+    val name : String,
+    @DrawableRes var icon : Int
+)
