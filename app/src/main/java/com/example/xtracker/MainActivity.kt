1cc6af2d13d1aaf972eb14aed2abbcd56de3782e
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2024-05-21 18:43:08 +0200

    added navigation

--- app/src/main/java/com/example/xtracker/MainActivity.kt
+++ app/src/main/java/com/example/xtracker/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.xtracker.ui.theme.NavigationDrawerComposeTheme
 import kotlinx.coroutines.launch
 
@@ -19,6 +26,7 @@ class MainActivity : ComponentActivity() {
             NavigationDrawerComposeTheme {
                 val scaffoldState = rememberScaffoldState()
                 val scope = rememberCoroutineScope()
+                val navController = rememberNavController()
                 Scaffold(
                     scaffoldState = scaffoldState,
                     topBar = {
@@ -31,40 +39,63 @@ class MainActivity : ComponentActivity() {
                          )
                     },
                     drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                    content = {
+                        NavHost(navController = navController, startDestination = "dashboard") {
+                            composable("dashboard") {
+                                Text(text = "Dashboard")
+                            }
+                            composable("expenses") {
+                                Text(text = "Expenses")
+                            }
+                            composable("income") {
+                                Text(text = "Income")
+                            }
+                            composable("savings") {
+                                Text(text = "Savings")
+                            }
+                        }
+                    },
                     drawerContent = {
                         DrawerHeader()
                         DrawerBody(
                             items = listOf(
                                 MenuItem(
-                                    id = "home",
-                                    title = "Home",
-                                    contentDescription = "Go to home screen",
-                                    icon = Icons.Default.Home
+                                    id = "dashboard",
+                                    title = "Dashboard",
+                                    contentDescription = "Go to dashboard screen",
+                                    icon = Icons.Default.DateRange
                                 ),
                                 MenuItem(
-                                    id = "settings",
-                                    title = "Settings",
-                                    contentDescription = "Go to settings screen",
-                                    icon = Icons.Default.Settings
+                                    id = "income",
+                                    title = "Income",
+                                    contentDescription = "Go to income screen",
+                                    icon = Icons.Default.Email
                                 ),
                                 MenuItem(
-                                    id = "help",
-                                    title = "Help",
-                                    contentDescription = "Get help",
-                                    icon = Icons.Default.Info
+                                    id = "expenses",
+                                    title = "Expenses",
+                                    contentDescription = "Go to expenses screen",
+                                    icon = Icons.Default.ShoppingCart
+                                ),
+                                MenuItem(
+                                    id = "savings",
+                                    title = "Savings",
+                                    contentDescription = "Go to savings screen",
+                                    icon = Icons.Default.Lock
                                 ),
                             ),
                             onItemClick = {
-                                println("Clicked on ${it.title}")
+                                println("Clicked on ${it.id}")
+                                navController.navigate(it.id)
                                 scope.launch {
                                     scaffoldState.drawerState.close()
                                 }
                             }
                         )
                     }
-                ) {
+                )
 
-                }
+
             }
         }
     }
