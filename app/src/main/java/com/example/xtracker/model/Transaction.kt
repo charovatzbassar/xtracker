ea52c9bf9a4c097cba0999dc1311b1eb712cf8c0
charovatzbassar <carovac.basar@gmail.com>
2024-05-22 16:34:27 +0200

    added model

---
+++ app/src/main/java/com/example/xtracker/model/Transaction.kt
@@ -0,0 +1,11 @@
+package com.example.xtracker.model
+
+data class Transaction(
+    val id : Int,
+    val amount : Double,
+    val category : Category,
+    val date : String,
+    val type : Type
+)
+
+
