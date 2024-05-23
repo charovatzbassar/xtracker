3bd6816f152a2073f461f633e16e6b99d7577d6e
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2024-05-23 19:36:45 +0200

    added total fetching

--- app/src/main/java/com/example/xtracker/model/dao/TransactionDao.kt
+++ app/src/main/java/com/example/xtracker/model/dao/TransactionDao.kt
@@ -7,6 +7,7 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import com.example.xtracker.model.TransactionType
 import com.example.xtracker.model.models.Transaction
 import kotlinx.coroutines.flow.Flow
 
@@ -27,6 +28,18 @@ interface TransactionDao {
     @Query("SELECT * FROM `Transaction` WHERE transactionID = :id")
     suspend fun getTransaction(id: Int): Flow<Transaction>
 
+    @Query("SELECT * FROM `Transaction` WHERE type = :type")
+    suspend fun getTransactionsByType(type: TransactionType): Flow<List<Transaction>>
+
     @Query("SELECT * FROM `Transaction` WHERE categoryID = :categoryID")
     suspend fun getTransactionsByCategory(categoryID: Int): Flow<List<Transaction>>
+
+    @Query("SELECT SUM(amount) as total FROM `Transaction`")
+    suspend fun getTotal(): Flow<Int>
+
+    @Query("SELECT SUM(amount) as total FROM `Transaction` WHERE type = :type")
+    suspend fun getTotalForType(type: TransactionType): Flow<Int>
+
+    @Query("SELECT SUM(amount) as total FROM `Transaction` WHERE categoryID = :categoryID")
+    suspend fun getTotalForCategory(categoryID: Int): Flow<Int>
 }
--- app/src/main/java/com/example/xtracker/model/repositories/TransactionRepository.kt
+++ app/src/main/java/com/example/xtracker/model/repositories/TransactionRepository.kt
@@ -1,6 +1,7 @@
 
 package com.example.xtracker.model.repositories
 
+import com.example.xtracker.model.TransactionType
 import com.example.xtracker.model.dao.TransactionDao
 import com.example.xtracker.model.models.Transaction
 import kotlinx.coroutines.flow.Flow
@@ -16,5 +17,9 @@ class TransactionRepository(private val transactionDao: TransactionDao): BaseRep
 
     suspend fun getTransactions(): Flow<List<Transaction>> = transactionDao.getTransactions()
 
+    suspend fun getTotal(): Flow<Int> = transactionDao.getTotal()
+    suspend fun getTotalForType(type: TransactionType): Flow<Int> = transactionDao.getTotalForType(type)
+    suspend fun getTotalForCategory(categoryID: Int): Flow<Int> = transactionDao.getTotalForCategory(categoryID)
+    suspend fun getTransactionsByType(type: TransactionType): Flow<List<Transaction>> = transactionDao.getTransactionsByType(type)
     suspend fun getTransactionsByCategory(categoryID: Int): Flow<List<Transaction>> = transactionDao.getTransactionsByCategory(categoryID)
 }
