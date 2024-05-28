026f442f8398bcaae3e18419b99f867dc76a0f04
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2024-05-29 00:22:07 +0200

    toolbar removed

--- app/src/main/java/com/example/xtracker/ui/composable/AppBar.kt
+++ app/src/main/java/com/example/xtracker/ui/composable/AppBar.kt
@@ -2,13 +2,15 @@ import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.xtracker.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyAppBar(onNavigationIconClick: () -> Unit) {
     TopAppBar(
         title = {
-            Text(text = "XTracker")
+            Text(text = stringResource(id = R.string.app_name))
         },
         navigationIcon = {
             IconButton(onClick = onNavigationIconClick) {
