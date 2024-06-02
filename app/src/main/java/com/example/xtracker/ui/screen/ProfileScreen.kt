96f548095d2cd9937addc24e5f8332d3c614278d
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2024-06-02 19:00:38 +0200

    modification

--- app/src/main/java/com/example/xtracker/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/xtracker/ui/screen/ProfileScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,9 +21,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.xtracker.ui.utils.getIconForType
 import com.example.xtracker.viewModel.UserViewModel
 
 @Composable
@@ -36,37 +44,39 @@ fun ProfileScreen(userViewModel: UserViewModel, navController: NavHostController
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
+            Icon(imageVector = Icons.Default.Person, contentDescription = "Icon", modifier = Modifier.size(40.dp))
+            Text(
+                text = "User Profile",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
             Card(
-                modifier = Modifier.padding(vertical = 8.dp),
+                modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
 
             ) {
                 Column(
                     modifier = Modifier.padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     if (userState != null) {
                         Text(
                             text = "Username: ${userState?.username ?: "N/A"}",
-                            fontSize = 20.sp
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontWeight = FontWeight.Bold
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         Text(
                             text = "Email: ${userState?.email ?: "N/A"}",
-                            fontSize = 20.sp
+                            fontSize = 20.sp,
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontWeight = FontWeight.Bold
                         )
                     } else {
                         Text(text = "Loading...", fontSize = 20.sp)
                     }
                 }
-            }
-
-            Button(
-                onClick = {
-                    navController.navigate("dashboard") // Navigate back to dashboard
-                },
-                modifier = Modifier.padding(vertical = 16.dp)
-            ) {
-                Text(text = "Back to Dashboard")
             }
         }
     }
